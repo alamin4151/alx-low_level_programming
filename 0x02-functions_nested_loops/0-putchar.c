6d9fac32ba5db8d72cae45dbce52a3e024384c76
alamin4151 <thephysicist41@gmail.com>
2023-10-20 06:12:15 -0700

    first_task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ * @ch  - an integer holding variable
+ * Return:   returns 0 to show the end
+ */
+int main(void)
+{
+int ch;
+for (ch = '_'; ch <= 'p'; ch = ch + 17)
+putchar(ch);
+for (ch = 'u'; ch >= 't'; ch--)
+putchar(ch);
+for (ch = 'c'; ch <= 'h'; ch = ch + 5)
+putchar(ch);
+for (ch = 'a'; ch <= 'r'; ch = ch + 17)
+putchar(ch);
+
+return (0);
+}
+
